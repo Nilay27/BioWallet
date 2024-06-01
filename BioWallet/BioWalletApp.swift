516becb2070987cd3afcca919f3d624927fdf300
Kumar Nilay <kumarnilay27@gmail.com>
2024-06-01 13:48:29 +0530

    Initial Commit

---
+++ BioWallet/BioWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioWalletApp.swift
+//  BioWallet
+//
+//  Created by Kumar Nilay on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BioWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BioWallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BioWallet
+//
+//  Created by Kumar Nilay on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
